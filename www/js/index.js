ea5574791ed2d589d09f53cc16ddea66346fb081
makevoid <makevoid@gmail.com>
2019-11-01 02:48:44 +0000

    use number for x axis, fix labels on charts

--- www/js/index.js
+++ www/js/index.js
@@ -1,43 +1,41 @@
 "use strict"
-
 const JWT_TOKEN = window.JWT_TOKEN
 
+  const TICKINTERVAL = 1
+  const XAXISRANGE = 30
 
-const TICKINTERVAL = 86400000
-const XAXISRANGE   = 777600000
+  let lastDate = 0
+  let data = []
 
-let lastDate = 0
-let data = []
-
-const getDayWiseTimeSeries = (baseval, count, yrange) => {
-  let i = 0
-  while (i < count) {
-    let x = baseval
-    let y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min
-    data.push({
-      x, y
-    })
-    lastDate = baseval
-    baseval += TICKINTERVAL
-    i++
-  }
-}
-
-const getNewSeries = (baseval, yrange) => {
-  const newDate = baseval + TICKINTERVAL
-  lastDate = newDate
-
-  for(let i = 0; i< data.length - 10; i++) {
-    // we reset the x and y of the data which is out of drawing area to prevent memory leaks
-    data[i].x = newDate - XAXISRANGE - TICKINTERVAL
-    data[i].y = 0
+  const getDayWiseTimeSeries = (baseval, count, yrange) => {
+    let i = 0
+    while (i < count) {
+      let x = baseval
+      let y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min
+      data.push({
+        x, y
+      })
+      lastDate = baseval
+      baseval += TICKINTERVAL
+      i++
+    }
   }
 
-  return {
-    x: newDate,
-    y: Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min,
+  const getNewSeries = (baseval, yrange) => {
+    const newDate = baseval + TICKINTERVAL
+    lastDate = newDate
+
+    for(let i = 0; i< data.length - 30; i++) {
+      data[i].x = newDate - XAXISRANGE - TICKINTERVAL
+      data[i].y = 0
+    }
+
+    return {
+      x: newDate,
+      y: Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min,
+    }
   }
-}
+
 
 const resetData = _ => {
   const len = data.length
@@ -57,76 +55,74 @@ const chartOptions = {
   type: 'line',
   animations: animOptions,
   toolbar: {
-    show: false
-  },
-  zoom: {
-    enabled: false
-  }
-}
-
-const options = {
-  chart: chartOptions,
-  dataLabels: {
-    enabled: false,
-  },
-  stroke: {
-    curve: 'smooth',
-  },
-  title: {
-    text: 'Dynamic Updating Chart',
-    align: 'left',
-  },
-  markers: {
-    size: 0,
-  },
-  xaxis: {
-    type: 'datetime',
-    range: XAXISRANGE,
-  },
-  yaxis: {
-    max: 100,
-  },
-  legend: {
     show: false,
   },
+  zoom: {
+    enabled: false,
+  },
 }
 
-const request = (url, options) => (
-  new Promise((resolve, reject) => {
-    cordova.plugin.http.sendRequest(url, options, (resp) => {
-      // console.log(resp.data)
-      resolve(resp.data)
-    }, (resp) => {
-      console.log("ERROR:", resp.status)
-      console.log(resp.error)
-      reject(resp.error)
-    });
-  })
-)
-
-const getDongles = async () => {
-  const options = { method: 'get' }
-  const url = "https://api.commadotai.com/v1/me/devices"
-  const data = await request(url, options)
-  console.log("getDongles", data)
-  return data
-}
-
-// get carState etc. from Athena
-const getData = async (dongleId) => {
-  const service = "carState"
-  const params = {
-    method: "getMessage",
-    params: {"service": service, "timeout": 3000},
-    jsonrpc: "2.0",
-    id: 0
+  const options = {
+    chart: chartOptions,
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: 'smooth',
+    },
+    markers: {
+      size: 0,
+    },
+    xaxis: {
+      type: 'number',
+      range: XAXISRANGE,
+    },
+    yaxis: {
+      max: 100,
+    },
+    legend: {
+      show: false,
+    },
   }
-  const options = { method: 'post', data: params }
-  const url = `https://athena.comma.ai/${dongleId}`
-  const data = await request(url, options)
-  console.log("getData", data)
-  return data
-}
+
+
+  const request = (url, options) => (
+    new Promise((resolve, reject) => {
+      cordova.plugin.http.sendRequest(url, options, (resp) => {
+        // console.log(resp.data)
+        resolve(resp.data)
+      }, (resp) => {
+        console.log("ERROR:", resp.status)
+        console.log(resp.error)
+        reject(resp.error)
+      });
+    })
+  )
+
+  const getDongles = async () => {
+    const options = { method: 'get' }
+    const url = "https://api.commadotai.com/v1/me/devices"
+    const data = await request(url, options)
+    console.log("getDongles", data)
+    return data
+  }
+
+  // get carState etc. from Athena
+  const getData = async (dongleId) => {
+    const service = "carState"
+    const params = {
+      method: "getMessage",
+      params: {"service": service, "timeout": 3000},
+      jsonrpc: "2.0",
+      id: 0
+    }
+    const options = { method: 'post', data: params }
+    const url = `https://athena.comma.ai/${dongleId}`
+    const data = await request(url, options)
+    console.log("getData", data)
+    return data
+  }
+
 
 const testRequest = async () => {
   cordova.plugin.http.setHeader('Authorization', `JWT ${JWT_TOKEN}`)
@@ -164,30 +160,43 @@ const app = {
 
     // -------
 
-    getDayWiseTimeSeries(new Date('11 Feb 2017 GMT').getTime(), 10, {
+    getDayWiseTimeSeries(1, 30, {
       min: 10,
       max: 90,
     })
 
-    const clone = (obj) => Object.assign({}, obj)
+    const clone = (obj) => ( { ...obj }) // Object.assign({}, obj)
     const cloneArr = (arr) => new Array(...arr)
 
     const options1  = clone(options)
     const options2  = clone(options)
     const options3  = clone(options)
+    // options3.title.text = "speed"
     const data1     = cloneArr(data)
     const data2     = cloneArr(data)
     const data3     = cloneArr(data)
 
     options1.series = [{
-      data: data,
+      data: data1,
     }]
+    options1.title = {
+      text: "steering angle",
+      align: "left",
+    }
     options2.series = [{
-      data: data,
+      data: data2,
     }]
+    options2.title = {
+      text: "steering torque",
+      align: "left",
+    }
     options3.series = [{
-      data: data,
+      data: data3,
     }]
+    options3.title = {
+      text: "speed",
+      align: "left",
+    }
 
     const chart1 = new ApexCharts(
       document.querySelector(".chart1"),
